d678e3895986b41c14a26decae88aed8bdfec4c2
Peyman Daei Rezaei <88220773+Peyman2012@users.noreply.github.com>
2023-08-16 23:39:36 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
-
-def multiples():
+n = int(input("Enter a number:"))
+def multiples(num):
     i = 1
     sum = 0
-    while i < 1000:
+    while i < num:
         if (i % 3 == 0) or (i % 5 == 0) :
             sum = sum + i
         i += 1
-    print(sum)
+    return sum
 
-multiples()
+print(multiples(n))
