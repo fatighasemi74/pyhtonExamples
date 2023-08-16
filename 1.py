073b6d7cfeba7f6422078875560a1ed3af8c3278
Peyman Daei Rezaei <88220773+Peyman2012@users.noreply.github.com>
2023-08-16 23:44:17 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,13 @@
 n = int(input("Enter a number:"))
 def multiples(num):
+    list_n=[]
     i = 1
     sum = 0
     while i < num:
         if (i % 3 == 0) or (i % 5 == 0) :
             sum = sum + i
+            list_n.append(sum)
         i += 1
-    return sum
+    return list_n
 
 print(multiples(n))
